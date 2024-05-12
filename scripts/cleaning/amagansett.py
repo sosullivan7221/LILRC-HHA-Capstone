6148b13fea205eab553fa2826654923b75ee653a
sosullivan7221 <sosullivan7221@gmail.com>
2024-05-12 13:51:43 -0400

    updated cleaning file

--- scripts/cleaning/amagansett.py
+++ scripts/cleaning/amagansett.py
@@ -1,46 +1,59 @@
 import pandas as pd 
-from data.mappingFile import mapping
+import datetime
 
 ### Data model to try:
         # - Column 1: Library Name (string)
         # - Column 2: Employee Number (int)
         # - Column 2: Job Title (string)
-        # - Column 3: Part time/Full time (string)
+        # - Column 3: Part time (stirng)
         # - Column 4: Salary (int)
-        # - Column 5: Year (int)
+        # - Column 5: Hourly (int)
+        # - Column 6: Year (int)
 
 df_transformed_column_names = [
     'Library Name', 
     'Employee Number', 
     'Job Title', 
-    'Part time/Full time', 
-    'Salary', 
+    'Part time', 
+    'Salary',
+    'Hourly', 
     'Year']
+
+
 
 
 df = pd.read_csv('data/raw/Copy of Amagansett - Salary Data.csv')
 
 # Standardizing column names
 new_columns = {
-    "Title": "Library Name",
     "Starting Annual Salary": "Salary",
-    "Part time": "Part time/Full time"
+    "Starting Hourly Rate" : "Hourly"
 }
 
 df.rename(columns=new_columns, inplace=True)
 
+df.dtypes
+
 # Convert 'Salary' from string to int, removing commas
-df['Salary'] = df['Salary'].str.replace(',', '').astype(float)
+df['Salary'] = df['Salary'].astype(str).str.replace(',', '').astype(float)
+
+# Convert 'Hourly' from string to int, removing commas
+df['Hourly'] = df['Hourly'].astype(str).str.replace(',', '').astype(float)
 
 # Fill in missing values for 'Part time/Full time' based on 'Starting Hourly Rate'
-df['Part time/Full time'] = df['Starting Hourly Rate'].apply(lambda x: 'Part time' if pd.notna(x) else 'Full time')
+df['Part time'] = df['Hourly'].apply(lambda x: 'Yes' if pd.notna(x) else 'No')
 
 # Determine 'Employee Number' and 'Year'
 df['Employee Number'] = range(1, len(df) + 1)
-df['Year'] = 2024  # Assuming all data is from the year 2024
+today = datetime.date.today()
+df['Year'] = today.year  
+
+# Add Library Name as a column
+
+df['Library Name'] = 'Amagansett'
 
 # Selecting and reordering columns to match the desired format
-final_columns = ['Employee Number', 'Part time/Full time', 'Salary', 'Year']
+final_columns = ['Year', 'Library Name','Employee Number', 'Part time', 'Salary', 'Hourly',]
 df = df[final_columns]
 
 df
