cf20bc84851a2900c457029da0478201e03d169d
sosullivan7221 <sosullivan7221@gmail.com>
2024-08-04 11:15:40 -0400

    formatted script + accept mult files on st

--- streamlit.py
+++ streamlit.py
@@ -10,6 +10,7 @@ from scripts.streamlit.north_babylon_st import clean_north_babylon
 from scripts.streamlit.smithtown_non_prof_st import clean_smithtown_non_prof
 from scripts.streamlit.babylon_st import clean_bablyon
 from scripts.streamlit.smithtown_prof_st import clean_smithtown_prof
+from scripts.streamlit.formatted_st import clean_formatted
 import pandas as pd
 from io import StringIO
 
@@ -24,64 +25,104 @@ cleaning_functions = {
     'north_babylon' : clean_north_babylon,
     'smithtown non-professional' : clean_smithtown_non_prof,
     'babylon' : clean_bablyon,
-    'smithtown professional' : clean_smithtown_prof}
+    'smithtown professional' : clean_smithtown_prof,
+    'amagansett' : clean_formatted,
+    'bellmore' : clean_formatted,
+    'bethpage' : clean_formatted,
+    'bridgehampton' : clean_formatted,
+    'cold spring harbor' : clean_formatted,
+    'copiague' : clean_formatted,
+    'east islip' : clean_formatted,
+    'emma clark library' : clean_formatted,
+    'hauppauge' : clean_formatted,
+    'island park' : clean_formatted,
+    'mastics-moriches-shirley' : clean_formatted,
+    'montauk' : clean_formatted,
+    'north bellmore' : clean_formatted,
+    'rockville center' : clean_formatted,
+    'rosyln (bryant library)' : clean_formatted,
+    'sea cliff' : clean_formatted,
+    'seaford' : clean_formatted,
+    'shelter island' : clean_formatted,
+    'shelter rock' : clean_formatted,
+    'south country' : clean_formatted,
+    'unknown libraries' : clean_formatted}
 
 st.title('LILRC Salary Data Cleaning')
 
 ## create an file upload widget for .csv
-uploaded_file = st.file_uploader("Upload Excel Data", type=['xlsx'])
+uploaded_files = st.file_uploader("Upload Excel Data", type=['xlsx'], accept_multiple_files= True)
 
-## check if file is uploaded
-if uploaded_file is not None:
-    
-    ## get the file name of the uploaded file
-    file_name = uploaded_file.name
+dataframes = []
 
-    ## get the file extension
-    file_extension = file_name.split('.')[-1]
-
-    ## print object type of uploaded file
-    print('Object type: ', type(uploaded_file))
-
-    try:
-        if file_extension == 'xlsx':
-           if file_name == 'Babylon.xlsx':
-               df = pd.read_excel(uploaded_file, skiprows=4, usecols=[1,2,3,4])
-           elif file_name == 'Smithtown Professional.xlsx':
-               df = pd.read_excel(uploaded_file, skiprows=2)
-           else:
-               df = pd.read_excel(uploaded_file)
-        else:
-            print('Unknown file extension')
-    except Exception as e:
-        st.write(f'Error: {e}')
+for file in uploaded_files:
+    ## check if file is uploaded
+    if file is not None:
         
-if df is not None:
-    # Retrieve name of file
-    base_name = file_name.split('.')[0].lower()
-    
-    # Get corresponding cleaning function
-    cleaning_function = cleaning_functions.get(base_name)
-    
-    if cleaning_function:
-        try:
-            df_clean = cleaning_function(df)
-            csv = df_clean.to_csv(index=False)
-            st.download_button(label='Download cleaned data', data=csv, 
-                           file_name='cleaned_data.csv', mime='text/csv')
+        ## get the file name of the uploaded file
+        file_name = file.name
 
+        ## get the file extension
+        file_extension = file_name.split('.')[-1]
+
+        ## print object type of uploaded file
+        print('Object type: ', type(file))
+
+        try:
+            if file_extension == 'xlsx':
+                if file_name == 'Babylon.xlsx':
+                    df = pd.read_excel(file, skiprows=4, usecols=[1,2,3,4], engine='openpyxl')
+                elif file_name == 'Smithtown Professional.xlsx':
+                    df = pd.read_excel(file, skiprows=2, engine='openpyxl')
+                else:
+                    df = pd.read_excel(file, engine='openpyxl')
+            else:
+                print('Unknown file extension')
         except Exception as e:
             st.write(f'Error: {e}')
+            
+    if df is not None:
+        # Retrieve name of file
+        base_name = file_name.split('.')[0].lower()
+        
+        # Get corresponding cleaning function
+        cleaning_function = cleaning_functions.get(base_name)
+        
+        if cleaning_function:
+            try:
+                if cleaning_function == clean_formatted:
+                    df_clean = cleaning_function(df)
+                    df_clean['Library Name'] = base_name.capitalize()
+                    csv = df_clean.to_csv(index=False, encoding='utf-8')
+                    st.download_button(label='Download cleaned data', data=csv, 
+                            file_name='cleaned_data.csv', mime='text/csv')
+                else:    
+                    df_clean = cleaning_function(df)
+                    csv = df_clean.to_csv(index=False, encoding='utf-8')
+                    st.download_button(label='Download cleaned data', data=csv, 
+                            file_name='cleaned_data.csv', mime='text/csv')
 
+            except Exception as e:
+                st.write(f'Error: {e}')
 
+        # Delete this stuff later !
         st.write(f'File name: {file_name}')
         st.write(f'File extension: {file_extension}')
         st.write('Cleaned data:')
         st.write(df_clean)
         
         st.write('------------')
+        
+        dataframes.append(df_clean)
     else:
         st.error('No cleaning function for this file')
+
+if dataframes:        
+    final_data = pd.concat(dataframes, ignore_index=True)
+    st.write('Final Data')
+    st.write(final_data)
+else:
+    st.write('No data')
 
 # Run the app
 # streamlit run app.py
